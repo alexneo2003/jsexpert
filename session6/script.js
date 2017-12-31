abf0baf0ac278bb48aa22dbe5a8f8911cc1d1d4c
Alex Neo <alexneo2003@gmail.com>
2017-12-31 21:57:19 +0200

    Session 06 homework

---
+++ session6/script.js
@@ -0,0 +1,19 @@
+/**
+ * function set template.js path param
+ */
+(function(win) {
+  var params = {
+    states: {
+      url: "/",
+      template: "temlate.js"
+    }
+  };
+  function setStates(params) {
+    if (win && !win.params) {
+      win.params = params;
+    }
+  }
+  setStates();
+  console.log(params.states.template);
+});
+window;
